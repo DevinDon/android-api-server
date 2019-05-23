d74630543d07a83965dc481c4da6d56cab133dc4
IInfinity <DevinDon@Foxmail.com>
2019-05-23 19:10:29 +0800

    test(entity): update test of class UserEntity

--- src/test/java/red/don/api/android/entity/UserEntityTest.java
+++ src/test/java/red/don/api/android/entity/UserEntityTest.java
@@ -15,6 +15,7 @@ public class UserEntityTest {
   private String email = "email@email.com";
   private String name = "name";
   private String password = "password";
+  private long token = 1;
 
   @Test
   public void constructorWithNothing() {
@@ -22,6 +23,7 @@ public class UserEntityTest {
     assertNull("email should be null", entity.getEmail());
     assertNull("name shoule be null", entity.getName());
     assertNull("password should be null", entity.getPassword());
+    assertEquals("token should be 0", 0, entity.getToken());
   }
 
   @Test
@@ -30,14 +32,25 @@ public class UserEntityTest {
     assertEquals("email should be " + email, email, entity.getEmail());
     assertNull("name should be null", entity.getName());
     assertEquals("password should be " + password, password, entity.getPassword());
+    assertEquals("token should be 0", 0, entity.getToken());
   }
 
   @Test
-  public void constructorWithAll() {
+  public void constructorWithEmailAndNameAndPassword() {
     UserEntity entity = new UserEntity(email, name, password);
     assertEquals("email should be " + email, email, entity.getEmail());
     assertEquals("name should be " + name, name, entity.getName());
     assertEquals("password should be " + password, password, entity.getPassword());
+    assertEquals("token should be 0", 0, entity.getToken());
+  }
+
+  @Test
+  public void constructorWithEmailAndNameAndPasswordAndToken() {
+    UserEntity entity = new UserEntity(email, name, password, token);
+    assertEquals("email should be " + email, email, entity.getEmail());
+    assertEquals("name should be " + name, name, entity.getName());
+    assertEquals("password should be " + password, password, entity.getPassword());
+    assertEquals("token should be " + token, token, entity.getToken());
   }
 
   @Test
@@ -49,19 +62,22 @@ public class UserEntityTest {
     assertEquals("name should be set", name, entity.getName());
     entity.setPassword(password);
     assertEquals("password should be set", password, entity.getPassword());
+    entity.setToken(token);
+    assertEquals("token should be " + token, token, entity.getToken());
   }
 
   @Test
   public void stringify() {
-    UserEntity entity = new UserEntity(email, name, password);
+    UserEntity entity = new UserEntity(email, name, password, token);
     assertEquals("stringify should work",
-        "UserEntity [email=" + email + ", name=" + name + ", password=" + password + "]", entity.toString());
+        "UserEntity [email=" + email + ", name=" + name + ", password=" + password + ", token=" + token + "]",
+        entity.toString());
   }
 
   @Test
   public void equalWithOther() {
-    UserEntity entity = new UserEntity(email, name, password);
-    UserEntity another = new UserEntity(email, name, password);
+    UserEntity entity = new UserEntity(email, name, password, token);
+    UserEntity another = new UserEntity(email, name, password, token);
     assertEquals("these two entities should be equal", entity, another);
   }
 
