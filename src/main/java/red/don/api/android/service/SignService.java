c51a3a109599077d3874eec6be1d4596eaed5812
IInfinity <DevinDon@Foxmail.com>
2019-05-23 19:08:26 +0800

    refactor(service): class SignService
    refactor: signIn(UserEntity), signOut(UserEntity)

--- src/main/java/red/don/api/android/service/SignService.java
+++ src/main/java/red/don/api/android/service/SignService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import red.don.api.android.entity.UserEntity;
 import red.don.api.android.mapper.UserMapper;
+import red.don.api.android.util.JWTUtil;
 
 @Service
 public class SignService {
@@ -13,28 +14,37 @@ public class SignService {
   private UserMapper mapper;
 
   /**
-   * Sign in.
-   * 
+   * Sign in and return JWT token.
+   *
    * @param user User entity.
-   * @return Success or not.
+   * @return JWT token or null.
    */
-  public boolean signIn(UserEntity user) {
-    return mapper.countWhere("`email` = '" + user.getEmail() + "' AND `password` = '" + user.getPassword() + "'") != 0;
+  public String signIn(UserEntity user) {
+    UserEntity result = mapper
+        .selectOneWhere("`email` = '" + user.getEmail() + "' AND `password` = '" + user.getPassword() + "'");
+    if (result == null) {
+      return null;
+    } else {
+      result.setToken(System.currentTimeMillis());
+      mapper.update("email", result.getEmail(), result);
+      return JWTUtil.generate(result);
+    }
   }
 
   /**
-   * Sign out, empty method.
-   * 
+   * Sign out, set token to 0 in order to disable JWT token.
+   *
    * @param user User entity.
    * @return Success or not.
    */
   public boolean signOut(UserEntity user) {
-    return false;
+    user.setToken(0);
+    return mapper.update("email", user.getEmail(), user);
   }
 
   /**
    * Sign up.
-   * 
+   *
    * @param user User entity.
    * @return Success or not.
    */
