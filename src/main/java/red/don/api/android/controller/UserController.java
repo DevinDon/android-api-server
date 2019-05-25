8a695a0aa72bd2564a32f7c4a65cda56b2139961
IInfinity <DevinDon@Foxmail.com>
2019-05-25 13:48:47 +0800

    refactor(controller): class UserController
    refactor: delete(UserEntity), modify(UserEntity, UserEntity), view(UserEntity)

--- src/main/java/red/don/api/android/controller/UserController.java
+++ src/main/java/red/don/api/android/controller/UserController.java
@@ -1,12 +1,15 @@
 package red.don.api.android.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import red.don.api.android.aspect.AuthTarget;
+import red.don.api.android.aspect.AuthUser;
 import red.don.api.android.entity.Response;
 import red.don.api.android.entity.UserEntity;
 import red.don.api.android.service.UserService;
@@ -18,21 +21,21 @@ public class UserController {
   @Autowired
   private UserService service;
 
-  // TODO: Refactor by AOP
-  @PostMapping("/delete")
-  public Response<String> delete(@RequestBody UserEntity user) {
+  @AuthTarget
+  @DeleteMapping("/delete")
+  public Response<String> delete(@AuthUser UserEntity user) {
     return new Response<>(service.delete(user.getEmail()));
   }
 
-  // TODO: Refactor by AOP
-  @PostMapping("/modify/{email}")
-  public Response<String> modify(@PathVariable("email") String email, @RequestBody UserEntity user) {
-    return new Response<>(service.modify(email, user));
+  @AuthTarget
+  @PutMapping("/modify")
+  public Response<String> modify(@AuthUser UserEntity user, @RequestBody UserEntity modified) {
+    return new Response<>(service.modify(user.getEmail(), modified));
   }
 
-  // TODO: Refactor by AOP
-  @PostMapping("/view")
-  public Response<UserEntity> view(@RequestBody UserEntity user) {
+  @AuthTarget
+  @GetMapping("/view")
+  public Response<UserEntity> view(@AuthUser UserEntity user) {
     UserEntity content = service.view(user.getEmail());
     return new Response<UserEntity>(content != null, content);
   }
