f34ee914555686be0f99f38d75b545e5e1c72e06
IInfinity <DevinDon@Foxmail.com>
2019-05-25 13:46:50 +0800

    refactor(controller): class CheckController
    refactor: check(UserEntity), view(UserEntity)

--- src/main/java/red/don/api/android/controller/CheckController.java
+++ src/main/java/red/don/api/android/controller/CheckController.java
@@ -1,12 +1,14 @@
 package red.don.api.android.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import red.don.api.android.aspect.AuthTarget;
+import red.don.api.android.aspect.AuthUser;
 import red.don.api.android.entity.Response;
 import red.don.api.android.entity.UserEntity;
 import red.don.api.android.service.CheckService;
@@ -18,15 +20,15 @@ public class CheckController {
   @Autowired
   private CheckService service;
 
-  // TODO: Refactor by AOP
+  @AuthTarget
   @PostMapping("/in")
-  public Response<String> check(@RequestBody UserEntity user) {
+  public Response<String> check(@AuthUser UserEntity user) {
     return new Response<>(service.check(user));
   }
 
-  // TODO: Refactor by AOP
-  @PostMapping("/view/{year}/{month}/{day}")
-  public Response<String> view(@RequestBody UserEntity user, @PathVariable("year") int year,
+  @AuthTarget
+  @GetMapping("/view/{year}/{month}/{day}")
+  public Response<String> view(@AuthUser UserEntity user, @PathVariable("year") int year,
       @PathVariable("month") int month, @PathVariable("day") int day) {
     return new Response<>(service.view(user, year, month, day));
   }
