a895f1a1813c11c085a9e844a29f12abb8f18555
IInfinity <DevinDon@Foxmail.com>
2019-05-27 13:52:04 +0800

    perf(controller): class StaticController
    perf: index(HttpServletRequest)

--- src/main/java/red/don/api/android/controller/StaticController.java
+++ src/main/java/red/don/api/android/controller/StaticController.java
@@ -2,17 +2,21 @@ package red.don.api.android.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import red.don.api.android.service.LogService;
 
 @Controller
 public class StaticController {
 
-  private int total = 0;
+  @Autowired
+  private LogService service;
 
   @GetMapping("/")
   public String index(HttpServletRequest request) {
-    request.setAttribute("total", ++total);
+    request.setAttribute("total", service.getCount());
     return "index.html";
   }
 
